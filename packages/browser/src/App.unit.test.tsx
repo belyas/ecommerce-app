86f5e64288c953242ff1ac6f71d5e90f1f963a1d
yassine belkaid <yassine.belkaid87@gmail.com>
2020-02-09 21:01:29 +0100

    feat: add browser package

---
+++ packages/browser/src/App.unit.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('renders Welcome Yassine! text', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Welcome Yassine!/i);
+
+  expect(linkElement.textContent).toEqual('Welcome Yassine!');
+});
