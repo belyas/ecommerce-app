baf175e4be0e90fd8b48ec89dade75df9143e591
yassine belkaid <yassine.belkaid87@gmail.com>
2020-02-09 20:48:55 +0100

    chore: add jest config file

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: path.resolve(__dirname, 'packages/tsconfig.base.json'),
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+    '\\.(svg)$': '<rootDir>/.config/jest/assetsTransformer.ts',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testPathIgnorePatterns: ['node_modules'],
+  coveragePathIgnorePatterns: ['.*/lib/.*', '.*/build/.*'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  verbose: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/index.{ts,tsx}',
+    '!**/serviceWorker.ts',
+    '!**/setupTests.ts',
+    '!**/*.d.ts',
+  ],
+  moduleNameMapper: {
+    '\\.(scss|css)$': '<rootDir>/.config/jest/styleMock.ts',
+    '\\.svg$': '<rootDir>/.config/jest/fileMock.ts',
+  },
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 80,
+      lines: 70,
+      statements: 80,
+    },
+  },
+  // setupFiles: ['<rootDir>/.config/jest/setupFile.ts'],
+};
