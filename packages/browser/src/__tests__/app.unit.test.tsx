aea3b58dac3f0d7dae465705b9c48f52fe433558
yassine belkaid <yassine.belkaid87@gmail.com>
2020-02-16 15:33:07 +0100

    fix: rewritten files name & move tests to their own folder

--- packages/browser/src/__tests__/app.unit.test.tsx
+++ packages/browser/src/__tests__/app.unit.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../app';
 
-test('renders Welcome Yassine! text', () => {
+test('renders Welcome Yassine :) text', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Welcome Yassine \:\)/i);
 
